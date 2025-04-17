083a84662270674bc3ecc865f22461155a6e5822
s30175 <alex.biktimirov38@gmail.com>
2025-04-17 17:51:45 +0200

    Create prepareDataset(String path)

--- src/main/java/org/example/Bayes/PrepareDataset.java
+++ src/main/java/org/example/Bayes/PrepareDataset.java
@@ -1,25 +1,24 @@
 package org.example.Bayes;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PrepareDataset {
-    public static List<String[]> prepareDataset(String path){
+    public static List<String[]> prepareDataset(String path) {
         List<String[]> dataset = new ArrayList<>();
 
-        try(var lines = Files.lines(Path.of(path))){
+        try (var lines = Files.lines(Path.of(path))) {
             lines
-                    .map(line -> line.split(" "))
-                    .forEach(dataset::add);
-        }catch (Exception e){
+                    .skip(1)
+                    .filter(line -> !line.trim().isEmpty())
+                    .forEach(line -> dataset.add(line.split(" ")));
+        } catch (IOException e) {
             System.out.println("File not found");
             return null;
         }
-
-        List<String> trainDataSet = new ArrayList<>();
-        List<String> testDataSet = new ArrayList<>();
 
         return dataset;
     }
