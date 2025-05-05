0f83d2175223ed487ce5afd4b44b087c61e7b934
s30175 <alex.biktimirov38@gmail.com>
2025-05-05 13:07:10 +0200

    Fix the issue with parsing iris.csv

---
+++ src/main/java/org/example/KMeans/PrepareDataset.java
@@ -0,0 +1,25 @@
+package org.example.KMeans;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrepareDataset {
+    public static List<String[]> prepareDataset(String path) {
+        List<String[]> dataset = new ArrayList<>();
+
+        try (var lines = Files.lines(Path.of(path))) {
+            lines
+                    .skip(1)
+                    .filter(line -> !line.trim().isEmpty())
+                    .forEach(line -> dataset.add(line.split(",")));
+        } catch (IOException e) {
+            System.out.println("File not found");
+            return null;
+        }
+
+        return dataset;
+    }
+}
